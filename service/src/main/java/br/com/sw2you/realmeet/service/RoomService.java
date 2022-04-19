3af75c762363810ad1a3011be7ffaa42a38a2dda
antonio marcos alves de lima <antonio.malves.lima@gmail.com>
2022-04-19 18:02:25 -0300

    criacao da service que busca um room por id e uma excecao para room nao encontrada

---
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -0,0 +1,26 @@
+package br.com.sw2you.realmeet.service;
+
+import static java.util.Objects.requireNonNull;
+
+import br.com.sw2you.realmeet.domain.entity.Room;
+import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.exception.RoomNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository){
+        this.roomRepository = roomRepository;
+    }
+
+    public Room findById(Long id){
+        requireNonNull(id);
+        return roomRepository.findById(id).orElseThrow(
+            () ->  new RoomNotFoundException(String.format("Room com o id: %d não existe", id))
+        );
+    }
+
+}
